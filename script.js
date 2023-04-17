631ec241781eec13e51b51097ab6f37c8edef7dc
soosanna <84853503+soosanna@users.noreply.github.com>
2023-04-17 21:30:35 +0530

    attached calculator app

---
+++ script.js
@@ -0,0 +1,18 @@
+var output=document.getElementById("output-screen");
+function display(num){
+    output.value+=num;
+}
+function calculate(){
+    try{
+        output.value=eval(output.value);
+    }
+    catch(err){
+        alert("invalid");
+    }
+    }
+function clear(){
+    output.value= " ";
+}
+function del(){
+    output.value=output.value.slice(0,-1);
+}
